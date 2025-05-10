e9394e934c2671be76549f4ff555af9a9e65edbb
Randall Degges <r@rdegges.com>
2025-05-09 22:38:27 -0700

    Adding a 'create_admin' CLI command to promote or create an admin.

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,6 +3,7 @@ from cli.sync_podcast import sync_podcast
 from cli.sync_blog import sync_blog
 from cli.sync_youtube import sync_youtube
 from cli.routes import list_routes
+from cli.create_admin import create_admin
 
 # Export all commands for use in the app
-__all__ = ["init_db", "sync_podcast", "sync_blog", "sync_youtube", "list_routes"]
+__all__ = ["init_db", "sync_podcast", "sync_blog", "sync_youtube", "list_routes", "create_admin"]
